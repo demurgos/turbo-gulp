080a4513a778ecc5c60096487ffdb5713a532b3e
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2016-01-25 20:55:09 +0100

    Fixed exports

--- utils/git.js
+++ utils/git.js
@@ -4,12 +4,12 @@ var fs = require('fs');
 
 var execFileAsync = Promise.promisify(childProcess.execFile);
 
-exports.exec = function exec(cmd, args, options){
+function exec(cmd, args, options){
   args.unshift(cmd);
   return execFileAsync('git', args, options);
-};
+}
 
-exports.ensureCleanMaster = function ensureCleanMaster(options){
+function ensureCleanMaster(options){
   return exec('symbolic-ref', ['HEAD'])
     .then(function (stdout) {
       if (stdout.trim() !== 'refs/heads/master') {
@@ -22,30 +22,16 @@ exports.ensureCleanMaster = function ensureCleanMaster(options){
         throw new Error('Working copy is dirty');
       }
     });
-};
-
-/*
-
-  const ensureCleanBranch = () => {
-      if (getHashFor('HEAD') !== getHashFor(branch)) {
-          throw new UsageError(`You need to be on the "${ branch
-              }" branch to run this script`);
-      }
-      if (getHashFor(branch) !== getHashFor(`${ remoteName }/${ branch }`)) {
-          throw new UsageError('You need to push your changes first');
-      }
-      if (run('git status -s').length) {
-          throw new UsageError(
-              'You have uncommited changes! Commit them before running this script');
-      }
-  };
-
-*/
+}
 
 // checks if the tag exists
-exports.checkTag = function checkTag(tag){
+function checkTag(tag){
   return exec('tag', ['-l', tag])
     .then(function (stdout) {
       return !!stdout.trim().length;
     });
-};
+}
+
+exports.exec = exec;
+exports.ensureCleanMaster = ensureCleanMaster;
+exports.checkTag = checkTag;
--- utils/project.js
+++ utils/project.js
@@ -8,7 +8,7 @@ var git = require('./git');
 var readFile = Promise.promisify(fs.readFile);
 var writeFile = Promise.promisify(fs.writeFile);
 
-exports.ensureUnusedTag = function ensureUnusedTag(tag){
+function ensureUnusedTag(tag){
   return git.checkTag(tag)
     .then(function(exists){
       if(exists){
@@ -16,9 +16,9 @@ exports.ensureUnusedTag = function ensureUnusedTag(tag){
       }
       return true;
     });
-};
+}
 
-exports.release = function(version, locations){
+function release(version, locations){
   var tag = 'v'+version;
   var message = 'Release '+tag;
   return Promise.all([
@@ -37,56 +37,26 @@ exports.release = function(version, locations){
     }).then(function(){
       return git.exec('tag', ['-a', tag, '-m', message]);
     });
-};
+}
 
-exports.readPackage = function readPackage(locations){
+function readPackage(locations){
   return readFile(locations.getPackage()).then(JSON.parse);
-};
+}
 
-exports.writePackage = function writePackage(data, locations){
+function writePackage(data, locations){
   return writeFile(locations.getPackage(), JSON.stringify(data, null, 2));
-};
+}
 
 // type: major/minor/patch
-exports.getNextVersion = function getNextVersion(type, locations){
+function getNextVersion(type, locations){
   return readPackage(locations)
     .then(function(pkg){
       return semver.inc(oldVersion, pkg.version);
     });
-};
-
-gulp.task('release', function (done) {
-  runSequence('clean', 'build', function () {
-    var tag;
-    return ensureCleanMaster()
-      .then(ensureValidVersion)
-      .then(function (res) {
-        tag = res;
-        return new Promise(function (resolve, reject) {
-          inquirer.prompt([{
-            type: 'confirm',
-            name: 'release',
-            message: 'Ready to release ' + tag + '. Continue ?'
-          }], resolve);
-        });
-      })
-      .then(function (res) {
-        if(!res.release){
-          throw new Error('Aborting');
-        }
-        return execGit('tag', ['-a', tag, '-m', 'release '+tag]);
-      })
-      .asCallback(done);
-  });
-});
-
-gulp.task('release.patch', []);
-gulp.task('release.minor', []);
-gulp.task('release.major', []);
-
-function readJSON(file) {
-  return JSON.parse(fs.readFileSync(file));
 }
 
-
-
+exports.ensureUnusedTag = ensureUnusedTag;
+exports.release = release;
+exports.readPackage = readPackage;
+exports.writePackage = writePackage;
+exports.getNextVersion = getNextVersion;
