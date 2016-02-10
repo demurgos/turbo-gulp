8ecd77bcc247d1e64ff4893c51965a6b7c1cf01e
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2016-02-10 20:36:49 +0100

    Publish build and add typings for orchestrator and Q

--- dist/node/core/tasks/build.node-test.tsc.js
+++ dist/node/core/tasks/build.node-test.tsc.js
@@ -4,7 +4,7 @@ var tsc = require("gulp-typescript");
 var merge = require("merge2");
 var tsc_1 = require("../config/tsc");
 function registerTask(gulp, locations, options) {
-    var tscConfig = _.merge({}, tsc_1.default, options.tsc);
+    var tscConfig = _.assign({}, tsc_1.default, options.tsc);
     gulp.task("build.node-test.tsc", function () {
         var tsResult = gulp
             .src(locations.getTypescriptSources("node", false), {
