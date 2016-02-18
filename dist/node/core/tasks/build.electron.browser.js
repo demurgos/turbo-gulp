d77e31e184b1c810a1ce35d00861033cd85a85b7
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2016-02-18 17:47:14 +0100

    Fixed dest for build.electron.browser

--- dist/node/core/tasks/build.electron.browser.js
+++ dist/node/core/tasks/build.electron.browser.js
@@ -2,7 +2,7 @@ var path = require("path");
 function registerTask(gulp, locations, options) {
     gulp.task('build.electron.browser', ["build.browser"], function () {
         var browserInput = locations.getBuildDirectory("browser");
-        var browserOutput = path.resolve(locations.getBuildDirectory("browser"), "browser");
+        var browserOutput = path.resolve(locations.getBuildDirectory("electron"), "browser");
         return gulp.src([path.resolve(browserInput, '**/*')], { base: browserInput })
             .pipe(gulp.dest(browserOutput));
     });
--- src/core/tasks/build.electron.browser.ts
+++ src/core/tasks/build.electron.browser.ts
@@ -7,7 +7,7 @@ import Locations from "../config/locations";
 export default function registerTask (gulp: any, locations: Locations, options?: any) {
   gulp.task('build.electron.browser', ["build.browser"], function(){
     let browserInput = locations.getBuildDirectory("browser");
-    let browserOutput = path.resolve(locations.getBuildDirectory("browser"), "browser");
+    let browserOutput = path.resolve(locations.getBuildDirectory("electron"), "browser");
     return gulp.src([path.resolve(browserInput, '**/*')], {base: browserInput})
       .pipe(gulp.dest(browserOutput));
   });
