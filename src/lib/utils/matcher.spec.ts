cc62bc0e0091872176a32cede725dbd16ca41c57
Charles Samborski <demurgos@demurgos.net>
2016-12-10 18:06:31 +0100

    Update dependencies

---
+++ src/lib/utils/matcher.spec.ts
@@ -0,0 +1,21 @@
+import {assert} from "chai";
+import {Minimatch} from "minimatch";
+import * as matcher from "./matcher";
+
+describe("matcher.asString", function () {
+  const data: string[] = [
+    "",
+    "a",
+    "a/b",
+    "*",
+    "**",
+    "**/*"
+  ];
+
+  for (const pattern of data) {
+    it(`should preserve the pattern ${JSON.stringify(pattern)}`, function () {
+      const actual = matcher.asString(new Minimatch(pattern));
+      assert.equal(actual, pattern);
+    });
+  }
+});
