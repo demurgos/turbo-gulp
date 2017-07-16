63a9228e506fe9085471d2c1b04a37a4fff7800d
Charles Samborski <demurgos@demurgos.net>
2017-07-16 21:03:27 +0200

    Add `:fix:lint` task
    This commit adds the `fix:lint` task to solve some lint errors
    automatically.

--- src/lib/project-tasks/index.ts
+++ src/lib/project-tasks/index.ts
@@ -4,14 +4,15 @@ import * as bumpMajor from "./bump-major";
 import * as bumpMinor from "./bump-minor";
 import * as bumpPatch from "./bump-patch";
 import * as lint from "./lint";
+import * as lintFix from "./lint-fix";
 import * as tslintJson from "./tslint-json";
 
 export function registerAll(gulp: Gulp, project: ProjectOptions) {
   bumpMajor.registerTask(gulp, project);
   bumpMinor.registerTask(gulp, project);
   bumpPatch.registerTask(gulp, project);
-  // install.registerTask(gulp, project);
   lint.registerTask(gulp, project);
+  lintFix.registerTask(gulp, project);
   tslintJson.registerTask(gulp, project);
 }
 
@@ -20,5 +21,6 @@ export {
   bumpMinor,
   bumpPatch,
   lint,
+  lintFix,
   tslintJson,
 };
