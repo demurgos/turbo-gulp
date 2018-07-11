b3234072849b167a15aa310323f45cfd4fdb585a
Charles Samborski <demurgos@demurgos.net>
2018-07-11 20:01:04 +0200

    Pin version of the `esm` dependency
    This commit pins the version of `esm` to `3.0.56`. This is the last
    version supporting `require`'ing `.mjs` files.

--- src/e2e/node-lib/project/src/test/polynomial.spec.ts
+++ src/e2e/node-lib/project/src/test/polynomial.spec.ts
@@ -1,25 +1,25 @@
-import { assert } from "chai";
+import chai from "chai";
 import { Polynomial } from "../lib/polynomial";
 
 describe("Polynomial", function () {
   it("normalize []", function () {
     const p: Polynomial = new Polynomial([]);
-    assert.deepEqual(p.coefficients, []);
+    chai.assert.deepEqual(p.coefficients, []);
   });
   it("normalize [0]", function () {
     const p: Polynomial = new Polynomial([0]);
-    assert.deepEqual(p.coefficients, []);
+    chai.assert.deepEqual(p.coefficients, []);
   });
   it("normalize [0, 0]", function () {
     const p: Polynomial = new Polynomial([0, 0]);
-    assert.deepEqual(p.coefficients, []);
+    chai.assert.deepEqual(p.coefficients, []);
   });
   it("normalize [1, 0]", function () {
     const p: Polynomial = new Polynomial([1, 0]);
-    assert.deepEqual(p.coefficients, [1]);
+    chai.assert.deepEqual(p.coefficients, [1]);
   });
   it("normalize [0, 1]", function () {
     const p: Polynomial = new Polynomial([0, 1]);
-    assert.deepEqual(p.coefficients, [0, 1]);
+    chai.assert.deepEqual(p.coefficients, [0, 1]);
   });
 });
